1f128efb7b4e0f398ae800590a94a0938cc05243
alan pavičić <aka@gensym.net>
2019-08-29 09:01:38 +0200

    Add warning to installer before replacing `./emacs.d`

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
 EMACS_CFG_DIR=`dirname "$0"`
-echo $EMACS_CFG_DIR
+
+echo "THIS WILL DELETE ~/.emacs.d"
+read -p "ARE YOU SURE (type YES)? " choice
+case "$choice" in
+  YES ) echo "copying $EMACS_CFG_DIR -> ~/.emacs.d";;
+  * ) echo "leaving ...";;
+esac
+
 rm -rf ~/.emacs.d
 cp -r $EMACS_CFG_DIR ~/.emacs.d
