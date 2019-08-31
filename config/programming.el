b015b9d208b6937546edc8aab840a6b0106c7e5d
alan pavičić <aka@gensym.net>
2019-08-31 23:37:55 +0200

    Support for Rust
    Alongside with rust-mode, generic lsp and company modes are
    added. Currently the flymake is used for on-the-fly check so no
    additional packages are added

--- config/programming.el
+++ config/programming.el
@@ -1,2 +1,26 @@
+;; -*- lexical-binding: t; -*-
+
 ;; compile
 (setq compilation-scroll-output t)
+
+;; company
+(use-package company
+  :ensure t
+  :init (global-company-mode))
+
+;; lsp
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :config (progn
+            (setq lsp-enable-snippet nil)
+            (require 'lsp-clients)))
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+;; rust
+(use-package rust-mode
+  :ensure t
+  :hook (rust-mode . lsp))
