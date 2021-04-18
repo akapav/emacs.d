1567f4e671915659fa1c6ba1cd1722fd90ad17b3
alan pavičić <aka@gensym.net>
2021-04-18 21:22:54 +0200

    Switch to flycheck

--- config/programming.el
+++ config/programming.el
@@ -1,38 +1,29 @@
 ;; -*- lexical-binding: t; -*-
 
-;; projectile
-(use-package projectile
-  :config
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-  (projectile-mode +1))
-
-;;;(use-package counsel-projectile
-;;;  :straight t)
-
 ;; compile
 (use-package popwin
   :config (progn
             (popwin-mode 1)
-            (push '("*Cargo Check*" :height 20) popwin:special-display-config)))
+            (push '("*Cargo Check*" :height 30) popwin:special-display-config)))
 (setq compilation-scroll-output t)
 
 ;; company
 (use-package company
   :init (global-company-mode))
 
+;; flycheck
+(use-package flycheck
+  :init (global-flycheck-mode))
+
 ;; lsp
 (use-package lsp-mode
   :commands lsp)
-
-(use-package company-lsp
-  :commands company-lsp)
 
 ;; rust
 (use-package rust-mode
   :hook (rust-mode . lsp))
 
 (use-package toml-mode)
-
 
 (use-package cargo
   :hook ((rust-mode . cargo-minor-mode)
@@ -43,9 +34,6 @@
 ;; npm install -g --save typescript
 ;; npm install -g typescript-language-server
 ;;
-
 (add-hook 'js-mode-hook #'lsp)
-
 (use-package js-comint)
-
 (use-package json-mode)
