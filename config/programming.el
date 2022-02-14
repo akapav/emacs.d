b75a0f79511ede7cdfdfd28d34e8aa61fd5806c8
alan pavičić <aka@gensym.net>
2022-02-14 20:30:52 +0100

    Use and configure `rustic`
    To use `rustic` it is preferable to use `lsp-mode` and `flycheck` as
    well. Additionally, there is new custom LSP client for TRAMP
    connections and RLS.

--- config/programming.el
+++ config/programming.el
@@ -11,18 +11,30 @@
 (use-package company
   :init (add-hook 'after-init-hook 'global-company-mode))
 
-;;
-(use-package eglot
-  :custom (tab-width 4))
+;; rustic
+(use-package flycheck)
+(use-package lsp-mode
+  :config
+  (with-eval-after-load 'lsp-rust
+    (lsp-register-client
+     (make-lsp-client :new-connection (lsp-tramp-connection (lambda () lsp-rust-rls-server-command))
+                      :major-modes '(rust-mode rustic-mode)
+                      :remote? t
+                      :priority (if (eq lsp-rust-server 'rls) 1 -1)
+                      :initialization-options '((omitInitBuild . t)
+                                                (cmdRun . t))
+                      :notification-handlers (ht ("window/progress" 'lsp-clients--rust-window-progress))
+                      :action-handlers (ht ("rls.run" 'lsp-rust--rls-run))
+                      :library-folders-fn (lambda (_workspace) lsp-rust-library-directories)
+                      :initialized-fn (lambda (workspace)
+                                        (with-lsp-workspace workspace
+                                          (lsp--set-configuration
+                                           (lsp-configuration-section "rust"))))
+                      :server-id 'rls))))
 
-(defun find-projectile-project (dir)
-  (let ((root (locate-dominating-file dir ".projectile")))
-    (when root `(vc . ,root))))
+(use-package rustic)
 
-(add-hook 'project-find-functions 'find-projectile-project)
 
-;;
-(use-package rust-mode
-  :hook (rust-mode . eglot-ensure))
+
 
 (use-package toml-mode)
