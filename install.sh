318e92695c2169175f65b96265d77f2b25865ba0
alan pavičić <aka@gensym.net>
2023-01-02 20:27:38 +0100

    Remove apps from install script

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ mkdir -p ~/.emacs.d/config
 mkdir -p ~/.emacs.d/secrets
 ln -sf `pwd`/init.el ~/.emacs.d/
 ln -sf `pwd`/config/general.el ~/.emacs.d/config/
-ln -sf `pwd`/config/apps.el ~/.emacs.d/config/
 ln -sf `pwd`/config/programming.el ~/.emacs.d/config/
 ln -sf `pwd`/secrets/.authinfo.gpg ~/.emacs.d/secrets/
